bef58814e141b85ec98d18b4388c42517906a22e
furina <keoplyna123@gmail.com>
2026-08-18 11:00:03 +0700

    Edited the collections.

--- collection.config.js
+++ collection.config.js
@@ -8,16 +8,16 @@
 
 const collection = {
   // What does your archive collect? e.g. "Khmer Proverbs"
-  name: "My Collection",
+  name: "Pchum Ben Archive",
 
   // One sentence: what is this archive, and why does it matter?
-  description: "A living archive of a piece of Khmer culture, built for ICT 340.",
+  description: "Pchum ben is a major cambodian buddhist festival dedicated to remembering deceased relatives and ancestors. It is a time for Cambodians to pay respects to their loved ones who have passed away, and to make offerings to the spirits of the dead. The word 'Pchum' refers to coming together, while 'Ben' reers to ball/lump of food, traditionally rice balls.",
 
   // Who is building this archive? Your name, as you want it shown.
-  curator: "Your Name",
+  curator: "Keo Phlyna",
 
   // Where does your knowledge come from? A person, a place, a community.
-  source: "To be gathered",
+  source: "I have learned about Pchum Ben frommy family and my community. As i have atteneded the festival for many years, I have gained a deep understanding of its significance and traditions. Also, i have researched through various sources from articles, and online resources. ",
 };
 
 export default collection;
